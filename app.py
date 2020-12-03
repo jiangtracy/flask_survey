f8f7ee16e03a198fe87b779e45bc23a778cca8e5
Tracy Jiang <tracyjiangmx@gmail.com>
2020-12-03 15:37:32 -0800

    completed part 6 and 7

--- app.py
+++ app.py
@@ -8,19 +8,29 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-responses = []
-
+# session['responses'] = []
 
 @app.route('/')
 def display_survey_info():
     """Renders the survey start root page"""
-
+    session['responses'] = []
     return render_template('survey_start.html', survey=survey)
 
 
 @app.route('/questions/<int:question_idx>', methods=['GET', 'POST'])
 def display_survey_question(question_idx):
     """Renders the next survey question"""
+
+    #prevent user from answering the quesitions if survey is already completed.
+
+    if len(session['responses']) == len(survey.questions):
+        return redirect('/complete')
+
+    # protect questions and redirect user to the correct questions if necessary
+    if question_idx != len(session['responses']):
+        question_idx = len(session['responses'])
+        return redirect(f'/questions/{question_idx}')
+    
 
     question = survey.questions[question_idx]
     return render_template('question.html', question=question)
@@ -32,9 +42,15 @@ def save_answer():
     to next page or renders the completion page"""
 
     answer = request.form.get('answer')
+    print(answer)
+
+    responses = session['responses']
     responses.append(answer)
-    
-    next_idx = len(responses)
+    session['responses'] = responses
+
+    print(session['responses'])
+
+    next_idx = len(session['responses'])
 
     if next_idx < len(survey.questions):
         return redirect(f'/questions/{next_idx}')
