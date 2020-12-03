a1edb9e833a58327988fece4a96288827678a78d
Sean Kim <sean.m.kim@gmail.com>
2020-12-03 11:49:15 -0800

    Completed steps 1-5, answering question moves to next question or completion page

--- app.py
+++ app.py
@@ -12,10 +12,30 @@ responses = []
 
 @app.route('/')
 def display_survey_info():
+    """Renders the survey start root page"""
+
     return render_template('survey_start.html', survey=survey)
 
 
-@app.route('/questions/<int:question_idx>', methods=['POST'])
+@app.route('/questions/<int:question_idx>', methods=['GET', 'POST'])
 def display_survey_question(question_idx):
+    """Renders the next survey question"""
+
     question = survey.questions[question_idx]
     return render_template('question.html', question=question)
+
+
+@app.route('/answer', methods=['POST'])
+def save_answer():
+    """Saves the survey question answer and either redirects
+    to next page or renders the completion page"""
+    
+    answer = request.form.get('answer')
+    responses.append(answer)
+    
+    next_idx = len(responses)
+
+    if next_idx < len(survey.questions):
+        return redirect(f'/questions/{next_idx}')
+    else:
+        return render_template('completion.html')
